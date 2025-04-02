4aae52d3683b3a062d215289d1885850b393f46d
Hananeh <hannanehojaghi@gmail.com>
2025-04-02 19:41:58 +0330

    step one

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,44 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import db.*;
+import db.exception.*;
+import example.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+        Human[] humans = {
+                new Human("Gholi"),
+                new Human("Jamshid"),
+                new Human("Akbar"),
+        };
+
+        System.out.println("#### Test add method ####");
+        for (Human h : humans) {
+            System.out.println("Adding " + h.name + " to the database.");
+            Database.add(h);
+        }
+
+        for (Human h : humans) {
+            System.out.println("Id of \"" + h.name + "\" is " + h.id + ".");
+        }
+
+        System.out.println("\n#### Test get method ####");
+        int gholiId = humans[0].id;
+        Human gholi = (Human) Database.get(gholiId);
+        System.out.println("Successfully got " + gholi.name + " from the database.");
+
+        System.out.println("\n#### Test update method ####");
+        gholi.name = "Gholi Mohammadi";
+        Database.update(gholi);
+        Human gholiAgain = (Human) Database.get(gholiId);
+        System.out.println("Updated name: \"" + gholiAgain.name + "\".");
+
+        System.out.println("\n#### Test delete method ####");
+        int jamshidId = humans[1].id;
+        Database.delete(jamshidId);
+
+        try {
+            Human jamshid = (Human) Database.get(jamshidId);
+        } catch (EntityNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
